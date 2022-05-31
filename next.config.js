aeea5b398172afd4a3f4ce11b26bde0d300180a2
ylee2531 <ylee2531@gmail.com>
2022-05-31 13:42:23 +0900

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["page.js"],
+  ignorePatterns: ["styles.js"],
+  compiler: {
+    styledComponents: true,
+  },
+  productionBrowserSourceMaps: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
